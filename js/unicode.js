d6145955399566be8de5d0f5672b238c9dcc9f3b
Kevin Lanzon <kevinlanzon@gmail.com>
2016-01-31 18:00:25 +0000

    fixes typo

--- js/unicode.js
+++ js/unicode.js
@@ -2,7 +2,7 @@ function checkUnicode (string) {
 
   var search = string.search(/[^\x00-\x80]/g); // matches any character not in the ASCII range
 
-  if (search !== -1) { // returns -1 if unicode chars are present
+  if (search !== -1) { // returns -1 if unicode chars are not present
     return "There are unicode Characters in this text";
   } else {
     return "No unicode present in this text";
